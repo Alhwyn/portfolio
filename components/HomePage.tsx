629f90ae6f3621331124737e005f3134b58657db
alhwyn <alhwyndwyne0411@gmail.com>
2026-07-12 02:43:08 -0700

    feat: adding spacing

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -149,8 +149,9 @@ export function HomePage({ projects }: HomePageProps) {
             hosted a bunch of Cursor events. Made Cards Against Humanity but for
             your camera roll, PhotoBomb, 5k+ users. I&apos;m based in Victoria,
             BC on Vancouver Island, Canada.
-
-            if youre building something cool would love to try it out by email at alhwyn@alhwyn.com
+            <br />
+            <br />
+            If you're building something cool I'd love to try it out by email at alhwyn@alhwyn.com
           </p>
         </div>
       </div>
